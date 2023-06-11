5410aac08341a26ab879e5974a212f99ea185c0d
KUWAHARA <s1300052@u-aizu.ac.jp>
2023-06-11 16:16:14 +0900

    Greetings added

---
+++ Dice_Game_greeting.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+char name[10];
+printf("What is your name? \n");
+scanf("%s",name);
+printf("Hello, %s!\n",name);
+
+int i,j,n;
+srand((unsigned)time(NULL));
+
+printf("Rolling dice...\n");
+i = rand()%6+1;
+j = rand()%6+1;
+printf("Die 1: %d\n",i);
+printf("Die 2: %d\n",j);
+printf("Total value: %d\n",i+j);
+
+return 0;
+}
